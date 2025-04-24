5ffa8122ff1740e9616c6b918746997ba33dda92
ayushgupta9803 <ayushgupta9803@gmail.com>
2025-04-24 15:40:58 +0530

    final change

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import requests
+import requests 
 import datetime
 from flask import Flask, render_template, request, jsonify
 import json
--- send_alert.py
+++ send_alert.py
@@ -1,5 +1,5 @@
 import json
-import time
+import time 
 import datetime
 import requests
 import smtplib
